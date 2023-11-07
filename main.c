55d24f6ed2413af2c1ca07f101188a41908e605a
Odys6x <phoinexw@gmail.com>
2023-11-07 20:48:43 +0800

    Insert

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ void saveFile();
 void insertRecord();
 
 // Function to query a record
-
+void query();
 
 // Function to update a record's value
 
@@ -30,8 +30,11 @@ void insertRecord();
 int main() {
     char filename[30];
     int cmd;
-    char data[256][256] = { {0} }; // Initialize data with 0 (NULL)
+    char data[256][256] = { {0} };
+    char append[256] = {0};// Initialize data with 0 (NULL)
     int recordCount = 0;
+    char key[256];
+    char value[256];
 
     printf("Please enter a filename: \n");
     scanf("%s", filename);
@@ -43,8 +46,19 @@ int main() {
         scanf("%d", &cmd);
 
         if (cmd == 1) {
-            insertRecord(data, &recordCount, "sdddds", "1.456");
-            saveFile(filename, data, recordCount);
+                printf("Enter a key: \n");
+                scanf("%s", key); 
+                printf("Enter a value: \n");
+                scanf("%s", value);
+                insertRecord(append, &recordCount, key, value,data);
+                saveFile(filename, append, recordCount);
+           
+        }
+        else if (cmd == 2) {
+            printf("Enter a key: \n");
+            scanf("%s", key);
+            query(data,key,recordCount);
+
         }
         // Add more cases for other commands as needed
     }
@@ -59,7 +73,7 @@ int main() {
     return 0;
 }
 
-void openFile(char* filename, char (*data)[2][MAX_RECORDS], int* recordCount) {
+void openFile(char* filename, char(*data)[2][MAX_RECORDS], int* recordCount) {
     FILE* file;
     file = fopen(filename, "r");
 
@@ -78,13 +92,11 @@ void openFile(char* filename, char (*data)[2][MAX_RECORDS], int* recordCount) {
     }
 }
 
-void saveFile(const char* filename, char (*data)[2][MAX_RECORDS], int recordCount) {
+void saveFile(const char* filename, char(*append)[256], int recordCount) {
     FILE* file = fopen(filename, "a");
 
     if (file != NULL) {
-        for (int i = 0; i < recordCount; i++) {
-            fprintf(file, "%s %s\n", data[i][0], data[i][1]);
-        }
+        fprintf(file, "%s %s\n", append[0], append[1]);
         fclose(file);
         printf("Database saved.\n");
     }
@@ -94,30 +106,39 @@ void saveFile(const char* filename, char (*data)[2][MAX_RECORDS], int recordCoun
 }
 
 
-void insertRecord(char(*data)[2][MAX_RECORDS], int* recordCount, const char* key, const char* value) {
+void insertRecord(char(*append)[256], int* recordCount, const char* key, const char* value, char data[256][256]) {
     if (*recordCount >= MAX_RECORDS) {
         printf("Error: Maximum record count reached.\n");
         return;
     }
 
+
     for (int i = 0; i < *recordCount; i++) {
-        if (strcmp(data[i][0], key) == 0) {
+        if (strcmp(data[i][0], key[i]) == 0) {
             printf("The record with Key=%s already exists in the database.\n", key);
             return;
         }
     }
 
-    (*data)[*recordCount][0] = malloc(strlen(key)); // +1 for the null terminator
-    (*data)[*recordCount][1] = malloc(strlen(value));
-
-    if (data[*recordCount][0] == NULL || data[*recordCount][1] == NULL) {
-        printf("Memory allocation failed.\n");
-        return;
-    }
-
-    strcpy(data[*recordCount][0], key);
-    strcpy(data[*recordCount][1], value);
+    strcpy(append[0], key);
+    strcpy(append[1], value);
 
     (*recordCount)++;
     printf("A new record of Key=%s, Value=%s is successfully inserted.\n", key, value);
+}
+
+void query(char data[2][MAX_RECORDS], char key, int recordCount) {
+    int found = 0;
+        for (int x = 0; x < recordCount; x++) {
+            if (strcmp(data[x][0], key) == 0) {
+                found = 1;
+                printf("Query found!\n");
+                printf("Your search result of keyword: %s is %s\n", key, data[x][1]);
+
+                break;
+            }
+        }
+    if (found == 0) {
+        printf("Query not found, your search result of keyword %s is empty.\n", key);
+    }
 }
